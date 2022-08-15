768dfcfe612cee1ab86b391eac2119508e589239
AbelG101 <abelgebeyehu101@gmail.com>
2022-08-15 16:46:44 +0300

    fix linter errors

--- src/JS/statusUpdates.js
+++ src/JS/statusUpdates.js
@@ -2,7 +2,7 @@ import { getTasksList } from './taskOperations.js';
 
 const updateTaskStatus = (index) => {
   const tasksList = getTasksList();
-  tasksList[index].completed = tasksList[index].completed ? false : true;
+  tasksList[index].completed = !tasksList[index].completed;
   localStorage.setItem('tasks: ', JSON.stringify(tasksList));
 };
 
